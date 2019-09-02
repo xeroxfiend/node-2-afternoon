30f0a486e326405f9b2a988a5f509f3c84764233
Brian Lowe <mynameisboy@gmail.com>
2019-09-02 17:56:04 -0600

    finished the assignment

--- server/controllers/messages_controller.js
+++ server/controllers/messages_controller.js
@@ -1,25 +1,42 @@
-const messages = []
+const messages = [];
 
-let id = 0
+let id = 0;
 
 module.exports = {
-    create: (res, req) => {
-        const {text, time} = req.body
-        messages.push({id, text, time})
-        id++
-        res.status(200).send(messages)
-    },
+  create: (req, res) => {
+    const {text, time} = req.body;
+    messages.push({id, text, time});
+    id++;
+    res.status(200).send(messages);
+  },
 
-    read: (res, req) => {
-        res.status(200).send(messsages)
-    },
+  read: (req, res) => {
+    res.status(200).send(messages);
+  },
 
-    update: (res, req) => {
+  update: (req, res) => {
+    const {text} = req.body;
+    const updateId = req.params.id;
+    const messageIndex = messages.findIndex(
+      message => message.id === parseInt(updateId)
+    );
+    let message = messages[messageIndex];
 
-    },
+    (messages[messageIndex] = {
+      id: message.id,
+      text: text || message.text,
+      time: message.time
+    }),
+      res.status(200).send(messages);
+  },
 
-    delete: (res, req) => {
+  delete: (req, res) => {
+    const deleteId = req.params.id;
+    const messageIndex = messages.findIndex(
+      message => message.id === parseInt(deleteId)
+    );
+    messages.splice(messageIndex, 1)
 
-    }
-
-}
+    res.status(200).send(messages);
+  }
+};
--- server/index.js
+++ server/index.js
@@ -1,7 +1,20 @@
-const server = require('express')
+const express = require('express')
 const app = express()
-const SERVER_PORT = 5000
+const messagesCtrl = require('./controllers/messages_controller')
+const SERVER_PORT = 3001
+
+app.use(express.static(__dirname + '/../public/build'))
 
 app.use(express.json())
+
+app.post('/api/messages', messagesCtrl.create)
+
+app.get('/api/messages', messagesCtrl.read)
+
+app.put('/api/messages/:id', messagesCtrl.update)
+
+app.delete('/api/messages/:id', messagesCtrl.delete)
+
+
 
 app.listen(SERVER_PORT, () => console.log(`Listening on port ${SERVER_PORT}`))
